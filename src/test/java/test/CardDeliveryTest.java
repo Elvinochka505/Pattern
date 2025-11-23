90cf58e4f37242514e9423f82d6a1a85928026f6
Elvina <habibullinaelvina1@gmail.com>
2025-11-23 15:12:19 +0500

    7

--- src/test/java/test/CardDeliveryTest.java
+++ src/test/java/test/CardDeliveryTest.java
@@ -1,10 +1,9 @@
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.*;
+import ru.netology.delivery.data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
 
 
 
@@ -38,7 +37,7 @@ class CardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $(Selectors.byText("Запланировать")).click();
         $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification] .notification__content")
+        $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
 
@@ -47,8 +46,9 @@ class CardDeliveryTest {
         $(Selectors.byText("Запланировать")).click();
 
         $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату.Перепланировать?"))
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
+
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(text("Встреча успешно запланирована на  " + secondMeetingDate))
